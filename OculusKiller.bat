26e634d1c844b0e03a6aa06865e5239f02884eef
Owen Sullivan <sulliops@gmail.com>
2021-09-09 13:37:00 -0400

    Add v1.0.0 Files

---
+++ OculusKiller.bat
@@ -0,0 +1,40 @@
+@echo off
+CLS
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+taskkill /IM "OVRServiceLauncher.exe" /F
+taskkill /IM "OVRRedir.exe" /F
+taskkill /IM "OVRServer_x64.exe" /F
+exit
+::::::::::::::::::::::::::::
+REM Run shell as admin (example) - put here code as you like
+cmd /k
---
+++ README.txt
@@ -0,0 +1,22 @@
+==================================================
+
+Oculus Killer v1.0.0
+Created by Owen Sullivan, https://sulliops.co
+
+==================================================
+
+Oculus Killer is a simple program designed to kill all Oculus processes at once, invalidating the need to open Task Manager and kill each process individually.
+Oculus Killer will automatically request elevation, as OVR Service Launcher cannot be killed without administrator rights.
+
+DO NOT MOVE, REMOVE, OR RENAME "OculusKiller.bat" OR "OculusKiller.exe"!!!!
+THE EXE PROGRAM IS SIMPLY A WRAPPER FOR THE BATCH FILE, AND IT WILL NOT FUNCTION IF THE BATCH FILE IS NOT IN THE SAME LOCATION!!!!
+
+==================================================
+
+Please make sure you've downloaded this program directly from https://github.com/sulliops/OculusKiller
+Files from anywhere other than https://github.com/sulliops/OculusKiller, there's a chance the files are not genuine and could contain malware!
+
+Report malicious downloads to: me@sulliops.co, https://sulliops.co/#contact
+Questions? Contact me at: me@sulliops.co, https://sulliops.co/#contact
+
+==================================================
